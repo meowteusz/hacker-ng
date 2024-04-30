0021f489222e48666532acbadbb667f35ee803d4
Mateusz Ryczek <mateusz@u.northwestern.edu>
2024-04-30 11:35:01 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+// Import our custom CSS
+import "./scss/styles.scss";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
